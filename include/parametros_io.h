46f10d3ce879448b514af4d9cf7e6cef91ebf83e
lorencosantos <53624465+lorencosantos@users.noreply.github.com>
2020-06-26 07:46:01 -0300

    Update parametros_io.h

--- include/parametros_io.h
+++ include/parametros_io.h
@@ -2,13 +2,13 @@
 // Created by Xx220xX on 24/06/2020.
 //
 
-#ifndef IC_LORE_CONFIG_IO_H
-#define IC_LORE_CONFIG_IO_H
+#ifndef CONFIG_IO_H
+#define CONFIG_IO_H
 
-#include "../include/configuracoes.h"
+#include "configuracoes.h"
 
 // configuracao da rede , tamanho das camadas, nome do arquivo
 int salvar_parametros(config_rede * , int *,char *);
 //tamanho  das camadas, nome do arquivo
 config_rede * ler_parametros(int **,const char *);
-#endif //IC_LORE_CONFIG_IO_H
+#endif //CONFIG_IO_H
